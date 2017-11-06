8301acbe216e1ff2623310285e2df0ec35d52440
Karanveer Singh <singhkaranveer671@gmail.com>
2017-11-06 14:49:28 -0800

    Create reverse.js

---
+++ callbacks/reverse.js
@@ -0,0 +1,8 @@
+const getHTML = require('../moduleExport');
+
+function reverse (html) {
+  html = s.split("").reverse().join("");
+  console.log(html);}
+
+
+  getHTML('sytantris.github.io','/http-examples/step4.html', reverse);
