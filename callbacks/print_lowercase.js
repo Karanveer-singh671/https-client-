6f39d6ca65f0216a76392bf831bf093024a05d78
Karanveer Singh <singhkaranveer671@gmail.com>
2017-11-06 14:49:03 -0800

    Delete print_lowercase.js

--- callbacks/print_lowercase.js
+++
@@ -1,8 +0,0 @@
-const getHTML = require('../moduleExport');
-
-function printHTML (html) {
-  html.toLowerCase();
-  console.log(html);}
-
-
-  getHTML('sytantris.github.io','/http-examples/step4.html', printHTML);
